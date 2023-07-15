ef5bafcdc3d4fa2d299e31005e87e19802ced968
Ray Andrew <4437323+rayandrew@users.noreply.github.com>
2023-07-15 14:47:30 -0500

    move preprocess to prototypes

--- workflow/scripts/pipeline/num-rows.py
+++ workflow/scripts/pipeline/num-rows.py
@@ -6,11 +6,9 @@ import numpy as np
 import pandas as pd
 
 from src.dataset.generator import DistributionColumnBasedGenerator
-from src.drift_detection.voting import get_offline_voting_drift_detector
 from src.helpers.config import Config, assert_config_params
-from src.helpers.definitions import DD_DIST_COLUMN, DD_ID, Dataset, Snakemake
 from src.helpers.dataset import create_avalanche_classification_datasets
-from src.helpers.definitions import Dataset, Snakemake
+from src.helpers.definitions import DD_DIST_COLUMN, DD_ID, Dataset, Snakemake
 from src.helpers.features import get_features
 from src.helpers.scenario import train_classification_scenario
 from src.transforms.general import add_transform_to_feature_engineering
@@ -19,13 +17,12 @@ from src.utils.logging import logging, setup_logging
 if TYPE_CHECKING:
     snakemake: Snakemake = Snakemake()
 
-DIST_COLUMN = "dist_id"
 
 setup_logging(snakemake.log[0])
 log = logging.getLogger(__name__)
 
 
-def add_dist_label_row(data: pd.DataFrame, num_rows:int, start_from=0):
+def add_dist_label_row(data: pd.DataFrame, num_rows: int, start_from=0):
     distributions = np.zeros(len(data), dtype=int)
     print(data.columns)
     # Print the frequency of values
@@ -33,38 +30,38 @@ def add_dist_label_row(data: pd.DataFrame, num_rows:int, start_from=0):
     frequency = data["bucket_util_cpu"].value_counts()
     print(frequency)
 
-    log.info(f"Number of distributions: {(len(data) + num_rows - 1) // num_rows}")
+    log.info(
+        f"Number of distributions: {(len(data) + num_rows - 1) // num_rows}"
+    )
     for idx in range(len(data)):
         if idx % num_rows == 0 and idx != 0:
             start_from += 1
         distributions[idx] = start_from
 
-    data[DIST_COLUMN] = pd.Series(distributions)
+    data[DD_DIST_COLUMN] = pd.Series(distributions)
     # distribution_counts = data.groupby('dist_id').size()
     # print(distribution_counts.to_string(index=False))
     return data
 
 
-
-def num_rows_transform(config: Config, target_name: str):
+def num_rows_transform(config: Config):
     def transform(data: pd.DataFrame) -> pd.DataFrame:
-        data = add_dist_label_row(data, config.scenario.period)
+        data = add_dist_label_row(data, config.scenario.period)  # type: ignore
         return data
 
     return transform
 
 
 def get_dataset(config: Config, input_path: Path):
-
     feature_engineering = get_features(config)
-    num_rows_transformer = num_rows_transform(config, config.dataset)
+    num_rows_transformer = num_rows_transform(config)
     feature_engineering = add_transform_to_feature_engineering(
         feature_engineering,
         num_rows_transformer,
         pos=DD_ID,
         sections=["preprocess"],
     )
-    
+
     match config.dataset.name:
         case Dataset.ALIBABA:
             from src.dataset.alibaba.container_seventeen import (
@@ -79,7 +76,7 @@ def get_dataset(config: Config, input_path: Path):
                 data=input_path,
                 prototype=AlibabaContainerDatasetPrototype(),
                 target=feature_engineering.target_name,
-                dist_col=DIST_COLUMN,
+                dist_col=DD_DIST_COLUMN,
                 feature_engineering=feature_engineering,
                 train_ratio=config.train_ratio,
             )
@@ -96,7 +93,7 @@ def get_dataset(config: Config, input_path: Path):
                 data=input_path,
                 prototype=AzureVMDatasetPrototype(),
                 target=feature_engineering.target_name,
-                dist_col=DIST_COLUMN,
+                dist_col=DD_DIST_COLUMN,
                 feature_engineering=feature_engineering,
                 train_ratio=config.train_ratio,
             )
@@ -113,7 +110,7 @@ def get_dataset(config: Config, input_path: Path):
                 data=input_path,
                 prototype=GoogleSchedulerDatasetPrototype(),
                 target=feature_engineering.target_name,
-                dist_col=DIST_COLUMN,
+                dist_col=DD_DIST_COLUMN,
                 feature_engineering=feature_engineering,
                 train_ratio=config.train_ratio,
             )
@@ -127,8 +124,6 @@ def get_dataset(config: Config, input_path: Path):
         create_avalanche_classification_datasets(dataset),
         dataset[0].train.input_size,
     )
-
-
 
 
 def get_benchmark(dataset: Sequence[Any]):
